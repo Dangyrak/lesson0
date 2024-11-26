6351e6300b39d4afd764c70a2054dd0c993be7dc
Dangyrak <varanati2003@gmail.com>
2024-11-26 19:43:29 +0300

    Add files via upload

---
+++ peremennye.py
@@ -0,0 +1,4 @@
+first_count=12
+second_count=1.5
+name='Python'
+print('Курс:',name,',''всего задач',first_count,"," "затрачено часов:",second_count,','"среднее время выполнения",(1.5/12),'часа''.')
